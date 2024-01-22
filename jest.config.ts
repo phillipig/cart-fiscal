32f305a22005174f3f92e8bba64d11c5e0f805b8
Marcoant007 <marcoantnovo@gmail.com>
2024-01-22 16:46:40 -0300

    feat: Adding vitest how plataform test because, jest is very slow

--- jest.config.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    transform: { '^.+\\.ts?$': 'ts-jest' },
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    preset: 'ts-jest',
-    testEnvironment: 'node'
-};
